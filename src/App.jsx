84061737f84a32e020c019f1deb76512190702ee
Jonnad89 <jonnadvwork@gmail.com>
2022-07-04 19:37:23 -0300

    Creación de imagen con descripcion de celular, estilado con CSS

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import Cellphone from './components/Cellphone'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+     <Cellphone />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Cellphone.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Cellphone = () => {
+    return (
+        <div>
+            <div className="cell-contain">
+                <figure>
+                    <img src="https://www.muycomputer.com/wp-content/uploads/2021/09/APERTURA_IPHONE13_PROMAX.jpg" alt="" />
+                    <div className="capa">
+                        <h3>Iphone 13 PRO MAX</h3>
+                        <p>Pantalla: 6.7", 1284 x 2778 pixels
+                            Procesador: Apple A15 Bionic
+                            Almacenamiento: 128GB/256GB/512GB/1TB
+                            Expansión: sin microSD
+                            Cámara: Cuádruple, 12MP+12MP +12MP+TOF 3D
+                            OS: iOS 15
+                            Peso: 240 g
+                            Precio: ~ 1099 USD</p>
+                    </div>
+                </figure>
+            </div>
+
+        </div>
+    );
+};
+
+export default Cellphone;
